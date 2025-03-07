73d3a73e55347def046fa40d43ae765d9700c21b
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2025-03-07 18:53:12 +0800

    Update data_process.py

--- Stable-Diffusion-Inpaint/data/data_process.py
+++ Stable-Diffusion-Inpaint/data/data_process.py
@@ -4,8 +4,9 @@ import glob
 # 定義目錄
 raw_dir = "./train_data/origin/"
 fixed_dir = "./train_data/fixed/"
-mask_dir = "./train_data/mask/"
-
+mask_dir = "./train_data/masks/"
+out_dir = "./train_data/"
+'''
 # 批量處理 raw
 for file_path in glob.glob(os.path.join(raw_dir, "*.jpg")):
     base_name = os.path.basename(file_path)  # 取得原始檔名
@@ -23,5 +24,14 @@ for file_path in glob.glob(os.path.join(mask_dir, "*.jpg")):
     base_name = os.path.basename(file_path)
     new_name = os.path.join(raw_dir, base_name.replace(".jpg", "_mask.jpg"))
     os.rename(file_path, new_name)
-
+'''
+raw_dir = "./train_data/reshaped/"
+for file_path in glob.glob(os.path.join(raw_dir, "*.jpg")):
+    base_name = os.path.basename(file_path)  # 取得原始檔名
+    new_name = os.path.join(out_dir, base_name)
+    os.rename(file_path, new_name)
+for file_path in glob.glob(os.path.join(mask_dir, "*.jpg")):
+    base_name = os.path.basename(file_path)
+    new_name = os.path.join(out_dir, base_name.replace(".jpg", "_mask.jpg"))
+    os.rename(file_path, new_name)
 print("檔案重新命名完成！")
