1d6ca45bf0492e95688bbcdd6b58ae8cb9c22826
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2025-07-20 19:19:39 +0800

    Update openaimodel.py

--- Stable-Diffusion-Inpaint/ldm/modules/diffusionmodules/openaimodel.py
+++ Stable-Diffusion-Inpaint/ldm/modules/diffusionmodules/openaimodel.py
@@ -345,7 +345,7 @@ class AttentionBlock(nn.Module):
         else:
             self.qkv = conv_nd(1, channels, channels * 3, 1)
             # self.proj_out = zero_module(conv_nd(1, channels, channels, 1))
-            self.proj_out = zero_module(conv_nd(2, channels, channels, 1))
+            self.proj_out = zero_module(conv_nd(1, channels, channels, 1))
         if use_new_attention_order:
             # split qkv before split heads
             self.attention = QKVAttention(self.num_heads)
