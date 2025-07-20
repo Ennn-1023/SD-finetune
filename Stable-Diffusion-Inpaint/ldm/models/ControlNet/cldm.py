39d79c7140d1ce10e2280ffef499bb8a9f5b7eea
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2025-07-20 12:55:37 +0800

    Update cldm.py

--- Stable-Diffusion-Inpaint/ldm/models/ControlNet/cldm.py
+++ Stable-Diffusion-Inpaint/ldm/models/ControlNet/cldm.py
@@ -556,7 +556,7 @@ class ControlLDMInPaintConcat(LatentDiffusion):
         if cond['c_concat'] is None:
             eps = diffusion_model(x=x_noisy, timesteps=t, context=cond_txt, control=None, only_mid_control=self.only_mid_control)
         else: # with concat conditioning as hint, not control key
-            control = self.control_model(x=x_noisy, hint=torch.cat(cond['c_concat'], 1), timesteps=t, context=cond_txt)
+            control = self.control_model(x=x_noisy, hint=cond['c_concat'], timesteps=t, context=cond_txt)
             control = [c * scale for c, scale in zip(control, self.control_scales)]
             eps = diffusion_model(x=x_noisy, timesteps=t, context=cond_txt, control=control, only_mid_control=self.only_mid_control)
 
