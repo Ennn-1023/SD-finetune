491d07481696f03cfd950a277c51e55dc26c4da8
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2025-07-20 19:13:35 +0800

    Update openaimodel.py

--- Stable-Diffusion-Inpaint/ldm/modules/diffusionmodules/openaimodel.py
+++ Stable-Diffusion-Inpaint/ldm/modules/diffusionmodules/openaimodel.py
@@ -364,6 +364,7 @@ class AttentionBlock(nn.Module):
         x = x.reshape(b, c, -1)
         qkv = self.qkv(self.norm(x))
         h = self.attention(qkv)
+        print(">> proj_out input shape:", h.shape)
         h = self.proj_out(h)
         return (x + h).reshape(b, c, *spatial)
 
